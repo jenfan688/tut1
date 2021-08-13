ccae5fe9b2a66af9359052dd298ec7f44e991827
jenfan688 <iyuga1314@hotmail.com>
2021-08-13 22:14:08 +1200

    Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting to") + ", " + name;
+    return messages.getString("greeting to") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
